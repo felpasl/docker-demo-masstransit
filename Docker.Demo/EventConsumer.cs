943e59ad3fa9873b30f7866427771476c01fcc1e
Felipe Lambert <felipe.lambert@outlook.com>
2021-12-30 10:09:51 -0300

    add Docker compose

---
+++ Docker.Demo/EventConsumer.cs
@@ -0,0 +1,17 @@
+﻿using MassTransit;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Docker.Demo
+{
+    public class EventConsumer :
+               IConsumer<ValueEntered>
+    {
+        public async Task Consume(ConsumeContext<ValueEntered> context)
+        {
+            Console.WriteLine("Value: {0}", context.Message.Value);
+        }
+    }
+}
---
+++ Docker.Demo/ValueEntered.cs
@@ -0,0 +1,7 @@
+﻿namespace Docker.Demo
+{
+    public class ValueEntered
+    {
+        public int Value { get; set; }
+    }
+}
